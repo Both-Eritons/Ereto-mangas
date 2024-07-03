b4f2a4ad70d0ac4a88cd6099941e530114c81c8d
Eriton <botheritons2005@gmail.com>
2024-07-03 13:04:51 -0300

    add UserModel

--- src/Api/Models/UserModel.php
+++ src/Api/Models/UserModel.php
@@ -2,6 +2,34 @@
 
 namespace Ereto\Api\Models;
 
-class UserModel
-{
+class UserModel {
+
+  private int $id, $username, $logo, $password;
+  function __construct(int $id = null, 
+    string $user, 
+    string $logo, 
+    string $pass) {
+
+    $this->id = $id;
+    $this->username = $user;
+    $this->logo = $logo;
+    $this->password = $pass;
+
+  }
+
+  function getUserId(): int {
+    return $this->id;
+  }
+
+  function getUsername(): string {
+    return $this->username;
+  }
+
+  function getLogo(): string {
+    return $this->logo;
+  }
+
+  function getPassword(): string {
+    return $this->password;
+  }
 }
