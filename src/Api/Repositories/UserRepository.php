056c9b47383b800c4ee8c49aaca0029c7574d651
Eriton <botheritons2005@gmail.com>
2024-07-05 08:38:47 -0300

    creating user Repository

--- src/Api/Repositories/UserRepository.php
+++ src/Api/Repositories/UserRepository.php
@@ -13,6 +13,26 @@ class UserRepository{
     $this->sql = Mysql::conn();
   }
 
+  function createUser(UserModel $user) {
+
+    $query = "INSERT INTO".$this->table."(username, password) VALUES(:username, :password)";
+
+    
+
+    $stmt = $this->sql->prepare($query);
+    $stmt->bindParam(":username", $user->getUsername());
+    $stmt->bindParam(":password", $user->getPassword());
+    $stmt->execute();
+
+    $row = $stmt->fetch();
+
+    if($row) {
+      return new UserModel($row["id"], $row["username"], $row["logo"], $row["password"]);
+    }
+
+    return null;
+  }
+
   function findUser($id) {
     $query = "SELECT * FROM ".$this->table." WHERE id = :id";
     $stmt = $this->sql->prepare($query);
