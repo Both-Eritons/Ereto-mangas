46f6a9b5b3b34e489680969bd779e2e710ee8d13
Eriton <botheritons2005@gmail.com>
2024-07-08 13:01:11 -0300

    Create and Find Service

--- src/Api/Services/MangaService.php
+++ src/Api/Services/MangaService.php
@@ -2,7 +2,10 @@
 
 namespace Ereto\Api\Services;
 
+use Ereto\Api\Models\MangaModel;
 use Ereto\Api\Repositories\MangaRepository;
+use Ereto\Constants\MangaConstant as Msg;
+use Exception;
 
 class MangaService {
 
@@ -11,7 +14,27 @@ class MangaService {
     $this->repo = $repo;
   }
 
-  function createManga($title) {
+  function createManga($title): ?MangaModel {
+    $mangaFound = $this->findMangaByTitle($title);
 
+    if($mangaFound) {
+      throw new Exception(Msg::$msg["READ"], 400);      
+    }
+
+    if(strlen($title) < 3) {
+      throw new Exception(Msg::$errors["MIN_TITLE"], 400);
+    }
+
+    $mangaM = new MangaModel(null, $title);
+    return $this->repo->createManga($mangaM);
   }
+
+  function findMangaById($id): ? MangaModel {
+    return $this->repo->findManga($id);
+  }
+
+  function findMangaByTitle($title): ? MangaModel {
+    return $this->repo->findMangaByTitle($title);
+  }
+
 }
