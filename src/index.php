d1ec3cfbbf0b9d4bbd8258e87e0d89ac9d3c0a8d
Eriton <botheritons2005@gmail.com>
2024-07-08 09:06:08 -0300

    creating Repository and Service of Manga

--- src/index.php
+++ src/index.php
@@ -3,9 +3,11 @@ header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json; charset=utf-8');
 require("vendor/autoload.php");
 
+use Ereto\Api\Controllers\MangaController;
 use MareaTurbo\Router;
 use Ereto\Api\Controllers\UserController;
 
 (new Router())->controllers([
-  UserController::class
+  UserController::class,
+  MangaController::class
 ]);
