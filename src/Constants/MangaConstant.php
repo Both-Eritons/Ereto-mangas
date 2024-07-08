ca4a9d30b4e36316c6e736b4c24597e280cec872
Eriton <botheritons2005@gmail.com>
2024-07-08 12:58:46 -0300

    change const to class

--- src/Constants/MangaConstant.php
+++ src/Constants/MangaConstant.php
@@ -2,10 +2,17 @@
 
 namespace Ereto\Constants;
 
-
-const Manga = [
-  "CREATED" => "Manga Criado",
-  "READ" => "Manga Encontrado",
-  "UPDATE" => "Manga Atualizado",
-  "DELETED" => "Manga Deletado",
-];
+class MangaConstant
+{
+  static $msg = [
+    "CREATED" => "Manga Criado",
+    "READ" => "Manga Encontrado",
+    "UPDATE" => "Manga Atualizado",
+    "DELETED" => "Manga Deletado",
+  ];
+  static $errors = [
+    "NOT_FOUND" => "Manga Não Encontrado",
+    "BLANK_TITLE" => "O Titulo nao pode ser em branco!",
+    "MIN_TITLE" => "o Titulo é muito curto"
+  ];
+}
