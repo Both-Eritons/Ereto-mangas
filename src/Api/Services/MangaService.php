53d72b2149ac6b23e60118de9e94a2692b73fa49
Eriton <botheritons2005@gmail.com>
2024-07-21 13:39:55 -0300

    formating files

--- src/Api/Services/MangaService.php
+++ src/Api/Services/MangaService.php
@@ -7,20 +7,23 @@ use Ereto\Api\Repositories\MangaRepository;
 use Ereto\Constants\MangaConstant as Msg;
 use Exception;
 
-class MangaService {
+class MangaService
+{
 
   private $repo, $arq;
-  function __construct(MangaRepository $repo) {
+  function __construct(MangaRepository $repo)
+  {
     $this->repo = $repo;
   }
 
-  function createManga($title): ?MangaModel {
+  function createManga($title): ?MangaModel
+  {
     $mangaFound = $this->findMangaByTitle($title);
-    if($mangaFound) {
-      throw new Exception(Msg::$msg["READ"], 409);      
+    if ($mangaFound) {
+      throw new Exception(Msg::$msg["READ"], 409);
     }
 
-    if(strlen($title) < 3) {
+    if (strlen($title) < 3) {
       throw new Exception(Msg::$errors["MIN_TITLE"], 400);
     }
 
@@ -31,46 +34,49 @@ class MangaService {
     return $manga;
   }
 
-  function findMangaById($id): ? MangaModel {
+  function findMangaById($id): ?MangaModel
+  {
     return $this->repo->findManga((int) $id);
   }
 
-  function findMangaByTitle($title): ? MangaModel {
+  function findMangaByTitle($title): ?MangaModel
+  {
     return $this->repo->findMangaByTitle($title);
   }
 
-  function searchManga($title): ? Array {
-    if(is_null($title)) {
+  function searchManga($title): ?array
+  {
+    if (is_null($title)) {
       throw new Exception(Msg::$errors["NOT_FOUND"], 404);
     }
 
     $re = $this->repo->searchManga($title);
 
-    if(is_null($re)) {
+    if (is_null($re)) {
       throw new Exception(Msg::$errors["NOT_FOUND"], 404);
     }
 
     return $re;
   }
 
-  function deleteMangaByTitle($title): ? MangaModel {
-    if(!$title) {
+  function deleteMangaByTitle($title): ?MangaModel
+  {
+    if (!$title) {
       throw new Exception(Msg::$errors["BLANK_TITLE"], 400);
     }
 
     $manga = $this->findMangaByTitle($title);
 
-    if(!$manga) {
+    if (!$manga) {
       throw new Exception(Msg::$errors["NOT_FOUND"], 404);
     }
 
     $manga = $this->repo->deleteMangaByTitle($title);
 
-    if(is_null($manga)) {
+    if (is_null($manga)) {
       throw new Exception(Msg::$errors["DELETE"], 400);
     }
 
     return $manga;
   }
-  
 }
